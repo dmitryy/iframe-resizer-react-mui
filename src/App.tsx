cc1328188d848a85544725acf8fa2fb1088e00a0
dmitryy <dmitry@ruswizards.com>
2022-07-26 20:30:43 +0300

    added iframe

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useRef } from 'react';
+import IframeResizer from 'iframe-resizer-react';
+import makeStyles from '@mui/styles/makeStyles';
+import { Grid, Paper } from '@mui/material';
+
+const useStyles = makeStyles({
+  container: {
+    padding: '10px',
+  },
+  iframe: {
+    minHeight: 1500,
+    width: 1,
+    minWidth: '100%',
+    border: 'none',
+  },
+});
 
 function App() {
+  const classes = useStyles();
+  const iframeRef = useRef(null);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Grid container spacing={2}>
+      <Grid item xs={12}>
+        <Paper className={classes.container}>
+          <IframeResizer
+            className={classes.iframe}
+            forwardRef={iframeRef}
+            heightCalculationMethod="lowestElement"
+            inPageLinks
+            log={false}
+            checkOrigin={false}
+            src={`https://dmitryy.github.io/iframe-resizer-react-content/`}
+          />
+        </Paper>
+      </Grid>
+    </Grid>
   );
 }
 
